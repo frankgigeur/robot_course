76367c0244b0e3d09cbf3b5022a107c8147b82da
ChuckNorris44 <milan.delev>
2021-10-05 16:14:23 -0400

    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 
-#define M_GAUCHE 0
-#define M_DROITE 1
-#define KP 0.00001
 
-bool test = true;
 
 void setup()
 {
@@ -16,30 +12,6 @@ void setup()
 void loop()
 {
 
-  if (test)
-  {
-    MOTOR_SetSpeed(M_GAUCHE,0.5);
-    MOTOR_SetSpeed(M_DROITE,0.2);
+  
 
-    CorrigerErreurMoteur(0.5);
-
-    test =false;
-  }
-
-  char str[50];
-
-  delay(1000);
-
-  sprintf(str, "Encodeur 1 : %ld\nEncodeur 2 : %ld\n %ld", ENCODER_Read(M_GAUCHE),ENCODER_Read(M_DROITE));
-
-
-}
-
-// Fonction corrigeant l'erreur du moteur droit en le considérant l'esclave du moteur gauche
-void CorrigerErreurMoteur(float vitesse)
-{
-  float errVitesse = (ENCODER_Read(M_GAUCHE)-ENCODER_Read(M_DROITE));
-  float corrErr = vitesse + (errVitesse*KP);
-
-  MOTOR_SetSpeed(M_DROITE,corrErr);
 }
